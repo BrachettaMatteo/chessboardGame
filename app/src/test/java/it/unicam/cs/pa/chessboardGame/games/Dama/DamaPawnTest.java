04b5aeaac77a3f5243ac18c1bf358b9e81b949a5
Matteo <matteo.brachetta@studenti.unicam.it>
2022-12-28 16:11:40 +0100

    DamaPawnTest optimize import assertions

--- app/src/test/java/it/unicam/cs/pa/chessboardGame/games/Dama/DamaPawnTest.java
+++ app/src/test/java/it/unicam/cs/pa/chessboardGame/games/Dama/DamaPawnTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * pawn test
